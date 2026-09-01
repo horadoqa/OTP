6c9737b1357cc9b0c70b5a33857c94acb0ea2afb
Ricardo Fahham <rfahham@hotmail.com>
2026-09-01 16:41:59 -0300

    feat: gera o OTP e salva no arquivo otp.json

--- gerar-otp.js
+++ gerar-otp.js
@@ -1,44 +1,24 @@
 const fs = require("fs");
 const crypto = require("crypto");
 
-const ARQUIVO_OTP = "otp.json";
-const INTERVALO = 30 * 1000; // 30 segundos
-
 function gerarOTP() {
   const array = new Uint32Array(1);
-
   crypto.webcrypto.getRandomValues(array);
 
   return String(array[0] % 1000000).padStart(6, "0");
 }
 
-function atualizarOTP() {
+const OTP_GERADO = gerarOTP();
 
-  const OTP_GERADO = gerarOTP();
+const dados = {
+  otp: OTP_GERADO,
+  criado_em: new Date().toISOString()
+};
 
-  const dados = {
-    otp: OTP_GERADO,
-    criado_em: new Date().toISOString()
-  };
+fs.writeFileSync(
+  "otp.json",
+  JSON.stringify(dados, null, 2),
+  "utf8"
+);
 
-  fs.writeFileSync(
-    ARQUIVO_OTP,
-    JSON.stringify(dados, null, 2),
-    "utf8"
-  );
-
-  console.log(
-    `[${new Date().toLocaleTimeString()}] OTP gerado: ${OTP_GERADO}`
-  );
-}
-
-// Gera imediatamente ao iniciar
-atualizarOTP();
-
-// Gera um novo OTP a cada 30 segundos
-setInterval(() => {
-  atualizarOTP();
-}, INTERVALO);
-
-console.log("Gerador de OTP iniciado.");
-console.log("Um novo código será gerado a cada 30 segundos.");
+console.log("OTP gerado:", OTP_GERADO);
